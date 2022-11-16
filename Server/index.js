09d4dcf439a4cc1968934197559548cdfc1f0159
yucai233 <2845039413@qq.com>
2022-11-16 22:21:04 +0800

    登录和注册逻辑

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,23 @@
-
-import http from 'http'
 import fs from 'fs'
 import express from 'express'
 import bodyParser from 'body-parser'
-import { MongoClient } from 'mongodb';
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url)
-const db = client.db();
-const connection = db.collection('cloudpan')
+import DbClient from 'ali-mysql-client'
 
+// connecting to mysql
+const db = new DbClient({
+    host: 'localhost',
+    user: 'root',
+    password: '20030606',
+    database: 'cloudisk'
+})
+
+
+// store path
 var userName = "";
 var currentPath = "D:/codelife/frontEnd/CloudPan/Server/disk/";
 
 
+// router
 const app = express()
 
 app.use(bodyParser.json())
@@ -25,17 +30,41 @@ app.get('/dir', (req, res) => {
     })
 })
 
-app.post('/loginUp', (req, res) => {
+app.post('/loginUp', async (req, res) => {
     console.log(req.body);
     let data = req.body;
     // TODO search infomation in mongodb, return the result of login
+    const result = await db
+        .select('*')
+        .from('user_info')
+        .where('name', data.name)
+        .where('password', data.password)
+        .queryRow();
+
+    console.log(result);
+    if(result === undefined) {
+        res.end('您还未注册')
+    }else {
+        res.end('登陆成功')
+    }
 })
 
-app.post('/regist', (req, res) => {
+app.post('/regist', async (req, res) => {
     let data = req.body;
-    connection.insertOne(data, (err, result) => {
-        
-    })
+    const result = await db
+        .select('*')
+        .from('user_info')
+        .where('name', data.name)
+        .queryRow();
+
+    console.log(result);
+
+    if(result !== undefined) res.end('用户名已存在')
+
+    await db
+        .insert("user_info", data)
+        .execute();
+    res.end('注册成功')
 })
 
 app.get('/createFolder', (req, res) => {
@@ -51,7 +80,16 @@ app.get('/openFile', (req, res) => {
     // TODO dealing empty folder 
 })
 
+app.get('uploudFile', (req, res) => {
+    // TODO recieve file uplouded by user
+})
+
+app.get('downloadFile', (req, res) => {
+    // TODO send file to client
+})
+
+
+// startup the server
 const server = app.listen(80, () => {
     console.log('process listening');
-    
 })
